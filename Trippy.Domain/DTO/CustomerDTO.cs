7b37405f779975e134f3f1eee5eb941e398b505e
tsaryananda@gmail.com <tsaryananda@gmail.com>
2025-10-07 14:54:06 +0530

    customer

--- Trippy.Domain/DTO/CustomerDTO.cs
+++ Trippy.Domain/DTO/CustomerDTO.cs
@@ -5,18 +5,18 @@
 
         public int CustomerId { get; set; } // Primary key
 
-        public required string CustomerName { get; set; }
-        public required string CustomerPhone { get; set; }
-        public required string CustomerEmail { get; set; }
-        public required string CustomerAddress { get; set; }
+        public  string CustomerName { get; set; } = "";
+        public  string CustomerPhone { get; set; } = "";
+        public string CustomerEmail { get; set; } = "";
+        public string CustomerAddress { get; set; }= "";
         public DateTime? DOB { get; set; }
         public String DOBString { get; set; } = "";
-        public required string Gender { get; set; }
-        public required string Nationality { get; set; }
+        public string Gender { get; set; } = "";
+        public string Nationality { get; set; } = "";
         public DateTime CreatedAt { get; set; }
 
         public bool IsActive { get; set; } = true;
 
-        public List<AuditLogDTO> AuditTrails { get; set; }
+        public  List<AuditLogDTO> AuditTrails { get; set; }
     }
 }
