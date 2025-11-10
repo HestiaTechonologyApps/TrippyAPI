816b76acb7d9a4979bbe245141963551042bc174
sreenathganga1988 <sreenath.sreeganga@gmail.com>
2025-11-10 15:12:57 +0530

    add

---
+++ Trippy.Core/Migrations/20251110055818_added expensse.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Trippy.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedexpensse : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ExpenseTypeCode",
+                table: "ExpenseTypes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ExpenseVoucher",
+                table: "ExpenseMasters",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentMode",
+                table: "ExpenseMasters",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RelatedEntityId",
+                table: "ExpenseMasters",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RelatedEntityType",
+                table: "ExpenseMasters",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpenseTypeCode",
+                table: "ExpenseTypes");
+
+            migrationBuilder.DropColumn(
+                name: "ExpenseVoucher",
+                table: "ExpenseMasters");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentMode",
+                table: "ExpenseMasters");
+
+            migrationBuilder.DropColumn(
+                name: "RelatedEntityId",
+                table: "ExpenseMasters");
+
+            migrationBuilder.DropColumn(
+                name: "RelatedEntityType",
+                table: "ExpenseMasters");
+        }
+    }
+}
