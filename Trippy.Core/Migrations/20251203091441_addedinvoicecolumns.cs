ecc923a661b711ce7326c96d337ab8dc0a3b7e3b
aryananda@hestiatechnology.com <aryananda@hestiatechnology.com>
2025-12-03 14:45:50 +0530

    added new fields

---
+++ Trippy.Core/Migrations/20251203091441_addedinvoicecolumns.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Trippy.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedinvoicecolumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CustomerId",
+                table: "InvoiceMasters",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "InvoiceDate",
+                table: "InvoiceMasters",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCompleted",
+                table: "InvoiceMasters",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomerId",
+                table: "InvoiceMasters");
+
+            migrationBuilder.DropColumn(
+                name: "InvoiceDate",
+                table: "InvoiceMasters");
+
+            migrationBuilder.DropColumn(
+                name: "IsCompleted",
+                table: "InvoiceMasters");
+        }
+    }
+}
